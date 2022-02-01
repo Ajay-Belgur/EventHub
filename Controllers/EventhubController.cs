2699f03f49a6df05ed24dac1487869571252ad86
ajaysbelgur <ajaysbelgur@gmail.com>
2022-02-01 09:44:37 +0530

    Add project files.

---
+++ Controllers/EventhubController.cs
@@ -0,0 +1,58 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Producer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHub.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventhubController : ControllerBase
+    {
+        private static string connectionString = "";
+        private static string eventhubName = "";
+        private EventHubProducerClient producerClient;
+        private EventHubConsumerClient consumerClient;
+
+        public EventhubController()
+        {
+            producerClient = new EventHubProducerClient(connectionString, eventhubName);
+            consumerClient = new EventHubConsumerClient("$Default", connectionString, eventhubName);
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<ActionResult> SendEvent(string item, int numberOfUnits)
+        {
+            using (var eventBatch = await producerClient.CreateBatchAsync())
+            {
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(new Order(item, numberOfUnits).ToString())));
+                await producerClient.SendAsync(eventBatch);
+            }
+
+            return Ok();
+        }
+
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<ActionResult> ReceiveEvent(string item, int numberOfUnits)
+        {
+            await foreach (PartitionEvent partitionEvent in consumerClient.ReadEventsAsync(new System.Threading.CancellationToken()))
+            {
+                EventData data = partitionEvent.Data;
+                return Ok(Encoding.UTF8.GetString(data.EventBody.ToArray()));
+            }
+
+            return Ok("No data");
+
+        }
+
+    }
+}
---
+++ Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventHub
+{
+    public class Order
+    {
+        private string itemName;
+        private int id;
+        private int numberOfItems;
+        public Order(string itemName, int numberOfItems)
+        {
+            this.itemName = itemName;
+            this.numberOfItems = numberOfItems;
+            this.id = new Random().Next(1000);
+
+        }
+        public override string ToString()
+        {
+            return $"Id : {id}, itemName : {itemName}, numberOfItems : {numberOfItems}";
+        }
+    }
+}
